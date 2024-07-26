4e00c5b186e6ae69031a68c56ed82414111e2380
Jovi Zulfikar <jovi_zulfikar@outlook.com>
2024-07-27 03:21:11 +0700

    add register client endpoint

--- src/main/java/com/example/oauth2/rest/controller/v1/UserController.java
+++ src/main/java/com/example/oauth2/rest/controller/v1/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 
     @PostMapping
     @ApiScope("oauth2.register-user")
-    public ResponseEntity<RegisterUserUseCase.Response> postMethodName(@RequestBody RegisterUserUseCase.Request request) {
+    public ResponseEntity<RegisterUserUseCase.Response> postUsers(@RequestBody RegisterUserUseCase.Request request) {
         var response = registerUserUseCase.registerUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
