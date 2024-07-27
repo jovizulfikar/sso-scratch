ccb19d55fb3130d03ffa3b993267760f280a386e
Jovi Zulfikar <muhammad.zulfikarrahman@dansmultipro.com>
2024-07-28 00:22:38 +0700

    adjust config properties name

--- src/main/java/com/example/sso/infra/config/AppSsoConfig.java
+++ src/main/java/com/example/sso/infra/config/AppSsoConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("openid")
+@ConfigurationProperties("sso")
 @Setter
 @Getter
 public class AppSsoConfig implements SsoConfig {
